cba1318bab322536f27a23edaa63ae82f18dca19
Manish sahu <122163664+Manishs2261@users.noreply.github.com>
2023-10-02 22:23:24 +0530

    manish03

---
+++ app/src/main/java/com/example/demosuraj/ButtonClass.java
@@ -0,0 +1,14 @@
+package com.example.demosuraj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_class);
+    }
+}
